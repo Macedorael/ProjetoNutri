063429092dedbd5dc6e71e53c7774f07de9f5302
Israel Macedo da Silva <raelmacedodasilva@gmail.com>
2025-02-28 15:17:17 -0300

    feat: ajuste

--- Models/ProjetoImcViewModel.cs
+++ Models/ProjetoImcViewModel.cs
@@ -8,15 +8,11 @@ namespace ProjetoNutri.Models
     public class ProjetoImcViewModel
     {
         public Projeto Projeto { get; set; }
-        public Imc Imc { get; set; } // IMC para ser criado
-<<<<<<< HEAD
+        public Imc Imc { get; set; }
         public List<Imc> Imcs { get; set; }
         public Circunferencia Circunferencia { get; set; }
         public List<Circunferencia> Circunferencias { get; set; }
         public Pregas Pregas { get; set; }
         public List<Pregas> Prega { get; set; }
-=======
- // Lista de IMCs associados ao projeto
-
     }
 }
